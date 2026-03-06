d817b085ef50b16875cd09d13ff631b8d1a5ae6e
Carlos Eduardo Rocha <rocha.cef@gmail.com>
2026-03-06 14:25:25 -0300

    refactor(backend): removing unused libraries

--- backend/Controllers/CategoriesController.cs
+++ backend/Controllers/CategoriesController.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 [ApiController]
 [Route("api/[controller]")]
